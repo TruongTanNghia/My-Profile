18d914771d1aeae5f63ba124bb947e86ea2c5c0e
TruongTanNghia <tannghiaxp@gmail.com>
2023-09-14 13:22:48 +0700

    Fix Bug #1

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import { NavBar } from './components/NavBar';
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from './components/Projects';
-
-import { Footer } from './components/Footer';
-=======
 import { Contact } from './components/Contact'
+import { Footer } from './components/Footer';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
